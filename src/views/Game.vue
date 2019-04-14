01ff81f05cbbcee9a4dcf0492f581f02d49aaf29
Orlando <edix_13apr@yahoo.com>
2019-04-14 20:11:46 +0300

    am adaugat caseta in care am intrebarea si butoanele de raspuns si am rezolvat ca atunci cand apas pe raspunsul corect sa imi treaca la runda urmatoare

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,9 +1,15 @@
 <template>
-    
+    <IntrebareRaspuns></IntrebareRaspuns>
 </template>
 
 <script>
+import IntrebareRaspuns from '@/components/IntrebareRaspuns.vue'
+import api from '@/API.js'
+
 export default {
+    components: {
+        IntrebareRaspuns
+    }
     
 }
 </script>
